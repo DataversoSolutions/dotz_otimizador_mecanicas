fba5cf36f4e369e056cccc905e77e661b7d52f47
Nilton Duarte <git@niltonduarte.com>
2022-10-28 19:57:37 -0300

    style - black'd fix - added a constraint to create_promo_interval_var

--- promo_scheduling/services/database.py
+++ promo_scheduling/services/database.py
@@ -26,21 +26,24 @@ class BigqueryDatabaseAdapter:
     @property
     def data_table_df(self):
         if self._data_table_df is None:
-            self._data_table_df = self.client.query(
+            query_response = self.client.query(
                 self.query.format(
                     project=self.project,
                     dataset_id=self.dataset_id,
-                    table_id=self.table_id
+                    table_id=self.table_id,
                 )
-            ).to_dataframe()
+            )
+            self._data_table_df = query_response.to_dataframe()
 
         return self._data_table_df
 
     def productivity_base(self, partner_name, mechanic_name):
-        df_filter = self.data_table_df.loc[(self.data_table_df['NomeParceiro'] == partner_name) & (
-            self.data_table_df['PromoAjust'].isin([mechanic_name]))]
-        if df_filter['DistinctDays'].sum() == 0:
+        df_filter = self.data_table_df.loc[
+            (self.data_table_df["NomeParceiro"] == partner_name)
+            & (self.data_table_df["PromoAjust"].isin([mechanic_name]))
+        ]
+        if df_filter["DistinctDays"].sum() == 0:
             result = 0
         else:
-            result = df_filter['Clientes'].sum() / df_filter['DistinctDays'].sum()
+            result = df_filter["Clientes"].sum() / df_filter["DistinctDays"].sum()
         return result
--- promo_scheduling/solver/solver.py
+++ promo_scheduling/solver/solver.py
@@ -1,4 +1,3 @@
-
 from typing import Dict, List
 from ortools.sat.python import cp_model
 import json
@@ -9,17 +8,18 @@ from promo_scheduling.entities.entity import (
     Mechanic,
     Partner,
     Promotion,
-    SystemSettings
+    SystemSettings,
 )
+from promo_scheduling.services.logging import logger
 
 
 class MechanicPartnerAssignmentSolver:
     def __init__(
-            self,
-            possible_promotions: List[Promotion],
-            partners: List[Partner],
-            mechanics: List[Mechanic],
-            system_settings: SystemSettings
+        self,
+        possible_promotions: List[Promotion],
+        partners: List[Partner],
+        mechanics: List[Mechanic],
+        system_settings: SystemSettings,
     ) -> None:
         self.possible_promotions = possible_promotions
         self.partners = partners
@@ -27,48 +27,55 @@ class MechanicPartnerAssignmentSolver:
         self.system_settings = system_settings
         self.model = cp_model.CpModel()
         self.solver = cp_model.CpSolver()
-        # self.solver.parameters.log_search_progress = True
+        self.solver.parameters.log_search_progress = True
         self.all_assignments: Dict[str, Assignment] = {}
 
     def create_promo_interval_var(self, relation_id, duration_horizon):
-        start_var = self.model.NewIntVar(0, duration_horizon, 'start_' + relation_id)
-        end_var = self.model.NewIntVar(0, duration_horizon, 'end_' + relation_id)
-        duration_var = self.model.NewIntVar(0, duration_horizon, 'duration_' + relation_id)
-        interval_var = self.model.NewIntervalVar(start_var, duration_var, end_var,
-                                                 'interval_' + relation_id)
+        start_var = self.model.NewIntVar(0, duration_horizon, "start_" + relation_id)
+        end_var = self.model.NewIntVar(0, duration_horizon, "end_" + relation_id)
+        duration_var = self.model.NewIntVar(
+            0, duration_horizon, "duration_" + relation_id
+        )
+        interval_var = self.model.NewIntervalVar(
+            start_var, duration_var, end_var, "interval_" + relation_id
+        )
         return start_var, end_var, duration_var, interval_var
 
     def create_promo_active_flag_var(self, relation_id, duration_var):
-        is_active = self.model.NewBoolVar(relation_id + '_is_active')
-        not_is_active = self.model.NewBoolVar(relation_id + '_not_is_active')
+        is_active = self.model.NewBoolVar(relation_id + "_is_active")
+        not_is_active = self.model.NewBoolVar(relation_id + "_not_is_active")
         # model can either be active or not
         self.model.Add(is_active + not_is_active == 1)
-        # if not active, duration should be zero, this is a workaround to set is active
+        # if not active, duration should be zero, this is a workaround to force the activation vars
         self.model.Add(duration_var == 0).OnlyEnforceIf(not_is_active)
+        self.model.Add(duration_var > 0).OnlyEnforceIf(is_active)
         return is_active, not_is_active
 
     def create_variables(self) -> None:
-        duration_horizon = max(promotion.mechanic.availability for promotion in self.possible_promotions)
+        duration_horizon = max(
+            promotion.mechanic.availability for promotion in self.possible_promotions
+        )
         for promotion in self.possible_promotions:
             partner = promotion.partner
             mechanic = promotion.mechanic
-            promo_availability = max(mechanic.availability, partner.availability)
-            relation_id = f'{partner.name}_{mechanic.name}'
+            schedule_dim_size = partner.availability
+            relation_id = f"{partner.name}_{mechanic.name}"
 
-            start_var, end_var, duration_var, interval_var = self.create_promo_interval_var(
-                relation_id=relation_id,
-                duration_horizon=duration_horizon
+            (
+                start_var,
+                end_var,
+                duration_var,
+                interval_var,
+            ) = self.create_promo_interval_var(
+                relation_id=relation_id, duration_horizon=duration_horizon
             )
 
             is_active, not_is_active = self.create_promo_active_flag_var(
-                relation_id=relation_id,
-                duration_var=duration_var
+                relation_id=relation_id, duration_var=duration_var
             )
 
             schedule = Schedule(
-                name=relation_id,
-                model=self.model,
-                length=promo_availability
+                name=relation_id, model=self.model, length=schedule_dim_size
             )
 
             self.all_assignments[relation_id] = Assignment(
@@ -80,30 +87,41 @@ class MechanicPartnerAssignmentSolver:
                 duration=duration_var,
                 interval=interval_var,
                 schedule=schedule,
-                promotion=promotion
+                promotion=promotion,
             )
 
-    def add_constraint_max_one_promo_per_partner(self) -> None:
+    def add_constraint_partner_max_availability(self) -> None:
         for partner in self.partners:
+            # the sum of all mechanics of the partner must be equals or lower than the partner availability
             self.model.Add(
-                sum(self.all_assignments[f'{partner.name}_{mechanic.name}'].interval.SizeExpr()
-                    for mechanic in self.mechanics) <= partner.availability
+                sum(
+                    self.all_assignments[
+                        f"{partner.name}_{mechanic.name}"
+                    ].interval.SizeExpr()
+                    for mechanic in self.mechanics
+                )
+                <= partner.availability
             )
 
     def add_constraint_promo_max_duration(self) -> None:
         for mechanic in self.mechanics:
+            # the sum of all partner with this mechanic must be equals or lower than the mechanic availability
             self.model.Add(
-                sum(self.all_assignments[f'{partner.name}_{mechanic.name}'].interval.SizeExpr()
-                    for partner in self.partners) <= mechanic.availability
+                sum(
+                    self.all_assignments[
+                        f"{partner.name}_{mechanic.name}"
+                    ].interval.SizeExpr()
+                    for partner in self.partners
+                )
+                <= mechanic.availability
             )
 
     def add_constraint_min_duration(self) -> None:
         for assignment in self.all_assignments.values():
+            # if the mechanic is active, we constraint its duration to be higher or equal the minimum duration
             self.model.Add(
                 assignment.interval.SizeExpr() >= self.system_settings.min_duration
-            ).OnlyEnforceIf(
-                assignment.is_active
-            )
+            ).OnlyEnforceIf(assignment.is_active)
 
     def add_schedule_constraint(self) -> None:
         for assignment in self.all_assignments.values():
@@ -120,25 +138,33 @@ class MechanicPartnerAssignmentSolver:
                 # only one day active
                 self.model.AddAtMostOne(day_flags)
                 # enforce duration if day is active
-                self.model.Add(sum(duration_array) == duration_var).OnlyEnforceIf(day_flags[day])
+                self.model.Add(sum(duration_array) == duration_var).OnlyEnforceIf(
+                    day_flags[day]
+                )
                 # enforce the duration array is a vector with all ones in the beginning
                 for pos in range(schedule_length - 1):
                     self.model.Add(duration_array[pos] >= duration_array[pos + 1])
 
-    def add_constraint_no_overlapping_promotion(self):
+    def add_constraint_no_overlapping_promotion_on_partner(self):
         for partner in self.partners:
+            # the partner cant have more than one promotion running at any time
+            # so, the intervals variables must not overlap
             self.model.AddNoOverlap(
-                self.all_assignments[f'{partner.name}_{mechanic.name}'].interval
+                self.all_assignments[f"{partner.name}_{mechanic.name}"].interval
                 for mechanic in self.mechanics
             )
 
     def add_constraint_promotion_end_before_availability_end(self):
         # we cant end a promotion after the partner availability
         for possible_assignment in self.all_assignments.values():
-            self.model.Add(possible_assignment.end <= possible_assignment.promotion.partner.availability)
+            self.model.Add(
+                possible_assignment.end
+                <= possible_assignment.promotion.partner.availability
+            )
 
     def create_objective_function(self) -> None:
         self.model.Maximize(
+            # we maximize the productivity (clients) of all promotions
             sum(
                 assignment.productivity()
                 for assignment in self.all_assignments.values()
@@ -146,22 +172,23 @@ class MechanicPartnerAssignmentSolver:
         )
 
     def has_solution_found(self) -> bool:
-        status_map = {
-            cp_model.UNKNOWN: 'UNKNOWN',
-            cp_model.MODEL_INVALID: 'MODEL_INVALID',
-            cp_model.FEASIBLE: 'FEASIBLE',
-            cp_model.INFEASIBLE: 'INFEASIBLE',
-            cp_model.OPTIMAL: 'OPTIMAL'
-        }
-        print(status_map[self.status])
+        # status_map = {
+        #     cp_model.UNKNOWN: 'UNKNOWN',
+        #     cp_model.MODEL_INVALID: 'MODEL_INVALID',
+        #     cp_model.FEASIBLE: 'FEASIBLE',
+        #     cp_model.INFEASIBLE: 'INFEASIBLE',
+        #     cp_model.OPTIMAL: 'OPTIMAL'
+        # }
+        # TODO: add this to log
+        # logger.debug(status_map[self.status])
         return self.status == cp_model.OPTIMAL or self.status == cp_model.FEASIBLE
 
     def run(self) -> None:
         self.create_variables()
         self.add_schedule_constraint()
-        self.add_constraint_max_one_promo_per_partner()
+        self.add_constraint_partner_max_availability()
         self.add_constraint_promo_max_duration()
-        self.add_constraint_no_overlapping_promotion()
+        self.add_constraint_no_overlapping_promotion_on_partner()
         self.add_constraint_min_duration()
         self.add_constraint_promotion_end_before_availability_end()
         self.create_objective_function()
@@ -170,77 +197,83 @@ class MechanicPartnerAssignmentSolver:
     def get_solution_json(self) -> str:
         solution = {}
         if not self.has_solution_found():
-            solution['status'] = "Solution not found"
+            solution["status"] = "Solution not found"
             return json.dumps(solution)
         promotion_assignments = []
         for promotion in self.possible_promotions:
             promotion_assignment = {}
             partner = promotion.partner
             mechanic = promotion.mechanic
-            assignment = self.all_assignments[f'{partner.name}_{mechanic.name}']
+            assignment = self.all_assignments[f"{partner.name}_{mechanic.name}"]
             start_var = self.solver.Value(assignment.start)
             end_var = self.solver.Value(assignment.end)
             productivity = self.solver.Value(assignment.productivity())
             if productivity == 0:
                 continue
-            promotion_assignment['partner'] = promotion.partner.name
-            promotion_assignment['mechanic'] = promotion.mechanic.name
-            promotion_assignment['start'] = start_var
-            promotion_assignment['end'] = end_var
-            promotion_assignment['productivity'] = productivity
+            promotion_assignment["partner"] = promotion.partner.name
+            promotion_assignment["mechanic"] = promotion.mechanic.name
+            promotion_assignment["start"] = start_var
+            promotion_assignment["end"] = end_var
+            promotion_assignment["productivity"] = productivity
             promotion_assignments.append(promotion_assignment)
-        solution['status'] = "Solution found"
-        solution['total_clients'] = self.solver.ObjectiveValue()
-        solution['promotion_assignments'] = promotion_assignments
+        solution["status"] = "Solution found"
+        solution["total_clients"] = self.solver.ObjectiveValue()
+        solution["promotion_assignments"] = promotion_assignments
         return solution
 
     def get_solution_str(self) -> str:
         result = []
         if self.has_solution_found():
-            result.append('Solution:')
+            result.append("Solution:")
 
             output = []
             for promotion in self.possible_promotions:
                 partner = promotion.partner
                 mechanic = promotion.mechanic
-                assignment = self.all_assignments[f'{partner.name}_{mechanic.name}']
+                assignment = self.all_assignments[f"{partner.name}_{mechanic.name}"]
                 start_var = self.solver.Value(assignment.start)
                 end_var = self.solver.Value(assignment.end)
                 productivity = self.solver.Value(assignment.productivity())
-                if productivity == 0:
-                    continue
+                # if productivity == 0:
+                #     continue
 
-                # schedule_matrix = [
-                #     [self.solver.Value(duration_array[i]) for i in range(len(duration_array))]
-                #     for duration_array in assignment.schedule.schedule_array
-                # ]
-                # is_active = self.solver.Value(assignment.is_active)
-                # not_is_active = self.solver.Value(assignment.not_is_active)
-                # result.append(f"{assignment.id} is active={is_active} / not is active={not_is_active}")
-                # for line in schedule_matrix:
-                #    result.append(line)
+                schedule_matrix = [
+                    [
+                        self.solver.Value(duration_array[i])
+                        for i in range(len(duration_array))
+                    ]
+                    for duration_array in assignment.schedule.schedule_array
+                ]
+                is_active = self.solver.Value(assignment.is_active)
+                not_is_active = self.solver.Value(assignment.not_is_active)
+                logger.debug(
+                    f"{assignment.id} is active={is_active} / not is active={not_is_active}"
+                )
+                for line in schedule_matrix:
+                    logger.debug(line)
                 output.append(
                     f"Parceiro {promotion.partner.name} "
                     f"com promoção {promotion.mechanic.name} "
                     f"iniciando em {start_var} e "
                     f"terminando em {end_var} "
-                    f"com resultando em {productivity} clientes")
+                    f"com resultando em {productivity} clientes"
+                )
 
-            result.append(f'Optimal result: {self.solver.ObjectiveValue()} clientes')
-            result.append('\n'.join(output))
+            result.append(f"Optimal result: {self.solver.ObjectiveValue()} clientes")
+            result.append("\n".join(output))
         else:
-            result.append('No solution found.')
-        return '\n'.join(result)
+            result.append("No solution found.")
+        return "\n".join(result)
 
     def print_solution(self) -> None:
         result = self.get_solution_str()
-        print(result)
+        logger.debug(result)
 
     def print_statistics(self) -> None:
-        print('\nStatistics')
-        print('  - conflicts: %i' % self.solver.NumConflicts())
-        print('  - branches : %i' % self.solver.NumBranches())
-        print('  - wall time: %f s' % self.solver.WallTime())
+        logger.debug("\nStatistics")
+        logger.debug("  - conflicts: %i" % self.solver.NumConflicts())
+        logger.debug("  - branches : %i" % self.solver.NumBranches())
+        logger.debug("  - wall time: %f s" % self.solver.WallTime())
 
     def export_model(self, filename) -> None:
         self.model.ExportToFile(filename)
